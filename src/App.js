67485a5c2ff492ca1b9fe741f03eacf75c2186bc
bigyanpoudel <bgyanpoudel11@gmail.com>
2020-11-28 19:46:09 +0545

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
+class App extends React.Component {
+  constructor(props)
+  {
+    super(props);
+    this.state={
+      counter: 0
+    }
+  }
+  render(){
+     return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1 data-test="component-app-increment">Display increment ${this.state.counter}</h1>
+        <button data-test="component-app-button" onClick={()=> this.setState({counter: this.state.counter + 1})}>Increment Button</button>
     </div>
   );
+  } 
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,48 @@
-import { render, screen } from '@testing-library/react';
+import React from 'React';
 import App from './App';
+import Enzyme,{shallow} from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+Enzyme.configure({ adapter: new EnzymeAdapter});
+/** 
+* Function to create shallowWrapper for App component
+*@function setup
+*@params {objects} props - props specific to setup
+*@params {object} state- initial state to setup
+*@return shallowWrapper
+*/ 
+const setup = (props={},state=null)=>{
+  const wrapper = shallow(<App {...props}/>);
+  if (state) wrapper.setState(state);
+  return wrapper;
+}
+
+const findTestByAttr = (wrapper,val)=>{
+  return wrapper.find(`[data-test="${val}"]`);
+}
+test('testing render without crashing', () => {
+  const wrapper = setup();
 });
+test('render increment button',()=>{
+   const wrapper = setup();
+   const button = findTestByAttr(wrapper,'component-app-button');
+   expect(button.length).toBe(1);
+});
+test('render increment display',()=>{
+    const wrapper = setup();
+    const increment = findTestByAttr(wrapper,'component-app-increment');
+    expect(increment.length).toBe(1);
+});
+test('counter start with 0',()=>{
+  const wrapper = setup();
+  const initialState = wrapper.state('counter');
+  expect(initialState).toBe(0);
+})
+test('check click display button',()=>{
+  const counter = 4;
+  const wrapper= setup(null,{counter});
+   const button = findTestByAttr(wrapper,'component-app-button');
+   button.simulate('click');
+  const increment = findTestByAttr(wrapper,'component-app-increment');
+  expect(increment.text()).toContain(counter + 1);
+})
